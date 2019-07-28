cacc711289ac86389d130116703df3d4707f2ed4
Espen Klem <espen.klem@gmail.com>
2019-07-28 15:03:08 +0200

    Formatting array output with JSON.stringifFormatting array output with JSON.stringifyy

--- demo/ngraminator-app.js
+++ demo/ngraminator-app.js
@@ -13,10 +13,11 @@ const calculateNgrams = function () {
   var text = document.getElementById("text").value.split(' ')
   var integerArray = document.getElementById("integerArray").value.split(',')
   console.log(text)
-  console.log(integerArray)
+  console.dir(integerArray)
   var ngrams = []
-  var ngrams = ngraminator(text, integerArray)
+  var ngrams = ngraminator(text, [3])
   console.log('N-grams: ' + ngrams)
+  ngrams = JSON.stringify(ngrams, 2, ' ')
   populateNgrams(ngrams)
 }
 
@@ -25,7 +26,7 @@ const calculateNgrams = function () {
 const populateNgrams = function(result) {
   console.log('Boom')
   console.dir(result)
-  const node = document.createElement('code')
+  const node = document.createElement('pre')
   node.innerHTML = result
   emptyElement('nGraminated')
   document.getElementById('nGraminated').appendChild(node)
